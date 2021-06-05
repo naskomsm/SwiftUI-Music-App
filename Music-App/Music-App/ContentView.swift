9bcbaf2e9a02b42d626e95a819a5c271f262967c
Atanas Kolev <atanaskolev@Atanass-Mac-mini.local>
2021-06-05 19:29:00 +0300

    Added project

---
+++ Music-App/Music-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music-App
+//
+//  Created by Atanas Kolev on 4.06.21.
+//  Copyright © 2021 Atanas Kolev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music-App/Music-AppTests/Music_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Music_AppTests.swift
+//  Music-AppTests
+//
+//  Created by Atanas Kolev on 4.06.21.
+//  Copyright © 2021 Atanas Kolev. All rights reserved.
+//
+
+import XCTest
+@testable import Music_App
+
+class Music_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
