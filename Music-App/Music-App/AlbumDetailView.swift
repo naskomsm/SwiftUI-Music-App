65f6e3467736e6852a2b4a12fbf89aea6843032a
Atanas Kolev <atanaskolev@Atanass-Mac-mini.local>
2021-06-06 12:48:51 +0300

    AlbumDetailView

---
+++ Music-App/Music-App/AlbumDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumDetailView.swift
+//  Music-App
+//
+//  Created by Atanas Kolev on 6.06.21.
+//  Copyright © 2021 Atanas Kolev. All rights reserved.
+//
+
+import SwiftUI
+
+struct AlbumDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumDetailView()
+    }
+}
