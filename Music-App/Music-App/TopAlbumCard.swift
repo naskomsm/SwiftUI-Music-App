24925b8e13da179c46b19ac3d637b7968ca211e2
Atanas Kolev <atanaskolev@Atanass-Mac-mini.local>
2021-06-06 12:41:29 +0300

    added TopAlbumCard and modified AlbumListView

---
+++ Music-App/Music-App/TopAlbumCard.swift
@@ -0,0 +1,21 @@
+//
+//  TopAlbumCard.swift
+//  Music-App
+//
+//  Created by Atanas Kolev on 6.06.21.
+//  Copyright © 2021 Atanas Kolev. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopAlbumCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopAlbumCard_Previews: PreviewProvider {
+    static var previews: some View {
+        TopAlbumCard()
+    }
+}
